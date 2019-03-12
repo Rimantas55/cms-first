702df194efb29676f24ba4c33a6b1e826df9f3b2
Rimantas55 <43905835+Rimantas55@users.noreply.github.com>
2019-03-12 18:44:27 +0200

    Making more secure db connection

--- includes/db.php
+++ includes/db.php
@@ -1,20 +1,27 @@
 <?php
 
-$db_user = 'localhost';
+$db["db_host"] = 'localhost';
+$db["db_user"] = 'root';
+$db["db_pass"] = '';
+$db["db_name"] = 'cms';
+
+foreach ($db as $key => $value) {
+
+	define(strtoupper($key), $value);
+
+}
 
 //Creating connection to MySql
-$connection = mysqli_connect('localhost', 'root', '', 'cms');
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
 //Checking if connection is working
 if($connection) {
 
-	echo " connection to database is successful" . "<br>";
+	echo '<div class="alert alert-success">Connection to database is successful</div>';
 } else {
 
 	die('Database connection failed');
 }
-
-
 
 
 
