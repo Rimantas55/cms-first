d40dcd4dc7f635eb11be939dd5d01b3a94ee180a
Rimantas55 <43905835+Rimantas55@users.noreply.github.com>
2019-03-13 23:54:21 +0200

    Update index.php

--- index.php
+++ index.php
@@ -28,35 +28,31 @@
                 $post_image = $row['post_image'];
                 $post_content = $row['post_content'];
 
-
-                
                 ?>
 
-    <h1 class="page-header">
-                Page Heading
-                <small>Secondary Text</small>
-            </h1>
+                <h1 class="page-header">
+                    Page Heading
+                    <small>Secondary Text</small>
+                </h1>
 
-            <!-- First Blog Post -->
-            <h2>
-                <a href="#"><?php $post_title; ?></a>
-            </h2>
-            <p class="lead">
-                by <a href="index.php"><?php $post_author; ?></a>
-            </p>
-            <p><span class="glyphicon glyphicon-time"></span> <?php $post_date; ?></p>
-            <hr>
-            <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-            <hr>
-            <p><?php $post_content; ?></p>
-            <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                <!-- First Blog Post -->
+                <h2>
+                    <a href="#"><?php echo $post_title; ?></a>
+                </h2>
+                <p class="lead">
+                    by <a href="index.php"><?php echo $post_author; ?></a>
+                </p>
+                <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date; ?></p>
+                <hr>
+                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+                <hr>
+                <p><?php echo $post_content; ?></p>
+                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
-            <hr>
-
-
+                <hr>
 
 
-        <?php    } ?>
+            <?php  } ?>
 
 
 
