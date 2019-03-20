14fac41d286f76b0a15c1869acbc406e63f942ff
Rimantas55 <43905835+Rimantas55@users.noreply.github.com>
2019-03-20 22:27:00 +0200

    finished custom searching engine

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,45 +1,11 @@
+     <!-- Blog Sidebar Widgets Column -->
      <div class="col-md-4">
-
-
-        <?php
-
-        if(isset($_POST['submit'])) {
-
-            $search = $_POST['search'];
-
-            $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search%' ";
-            $search_query = mysqli_query($connection, $query);
-
-        //checking if query connecting to post_tags
-            if(!$search_query) {
-
-                die("QUERY FAILED" . mysqli_error($connection));
-            }
-
-            $count = mysqli_num_rows($search_query);
-
-            if($count == 0) {
-
-                echo "<h1>NO RESULT</h1>";
-
-            } else {
-
-                echo "<div class='alert alert-success'>SOME RESULT </div>";
-            }
-
-        }
-
-
-
-        ?>
-
-
 
 
         <!-- Blog Search Well -->
         <div class="well">
             <h4>Blog Search</h4>
-            <form action="" method="post">
+            <form action="search.php" method="post">
                 <div class="input-group">
                     <input name="search" type="text" class="form-control">
                     <span class="input-group-btn">
@@ -51,12 +17,6 @@
             </form> <!--search form-->
             <!-- /.input-group -->
         </div>
-
-
-
-
-
-
 
 
         <!-- Blog Categories Well -->
