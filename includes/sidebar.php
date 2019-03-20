e5d38bc20bdc5f70b00515ac1fc9fc2598100e65
Rimantas55 <43905835+Rimantas55@users.noreply.github.com>
2019-03-20 21:32:45 +0200

    Update sidebar.php

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -5,78 +5,98 @@
 
         if(isset($_POST['submit'])) {
 
-        $search = $_POST['search'];
+            $search = $_POST['search'];
 
-     }
+            $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search%' ";
+            $search_query = mysqli_query($connection, $query);
 
+        //checking if query connecting to post_tags
+            if(!$search_query) {
 
+                die("QUERY FAILED" . mysqli_error($connection));
+            }
 
-     ?>
+            $count = mysqli_num_rows($search_query);
 
+            if($count == 0) {
 
+                echo "<h1>NO RESULT</h1>";
 
+            } else {
 
-     <!-- Blog Search Well -->
-     <div class="well">
-        <h4>Blog Search</h4>
-        <form action="" method="post">
-            <div class="input-group">
-                <input name="search" type="text" class="form-control">
-                <span class="input-group-btn">
-                    <button name="submit" type="submit" class="btn btn-default">
-                        <span class="glyphicon glyphicon-search"></span>
-                    </button>
-                </span>
-            </div>
-        </form> <!--search form-->
-        <!-- /.input-group -->
-    </div>
+                echo "<div class='alert alert-success'>SOME RESULT </div>";
+            }
+
+        }
 
 
 
+        ?>
 
 
 
 
-
-    <!-- Blog Categories Well -->
-    <div class="well">
-        <h4>Blog Categories</h4>
-        <div class="row">
-            <div class="col-lg-6">
-                <ul class="list-unstyled">
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.col-lg-6 -->
-            <div class="col-lg-6">
-                <ul class="list-unstyled">
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.col-lg-6 -->
+        <!-- Blog Search Well -->
+        <div class="well">
+            <h4>Blog Search</h4>
+            <form action="" method="post">
+                <div class="input-group">
+                    <input name="search" type="text" class="form-control">
+                    <span class="input-group-btn">
+                        <button name="submit" type="submit" class="btn btn-default">
+                            <span class="glyphicon glyphicon-search"></span>
+                        </button>
+                    </span>
+                </div>
+            </form> <!--search form-->
+            <!-- /.input-group -->
         </div>
-        <!-- /.row -->
-    </div>
 
-    <!-- Side Widget Well -->
-    <div class="well">
-        <h4>Side Widget Well</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
-    </div>
 
-</div>
+
+
+
+
+
+
+        <!-- Blog Categories Well -->
+        <div class="well">
+            <h4>Blog Categories</h4>
+            <div class="row">
+                <div class="col-lg-6">
+                    <ul class="list-unstyled">
+                        <li><a href="#">Category Name</a>
+                        </li>
+                        <li><a href="#">Category Name</a>
+                        </li>
+                        <li><a href="#">Category Name</a>
+                        </li>
+                        <li><a href="#">Category Name</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.col-lg-6 -->
+                <div class="col-lg-6">
+                    <ul class="list-unstyled">
+                        <li><a href="#">Category Name</a>
+                        </li>
+                        <li><a href="#">Category Name</a>
+                        </li>
+                        <li><a href="#">Category Name</a>
+                        </li>
+                        <li><a href="#">Category Name</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.col-lg-6 -->
+            </div>
+            <!-- /.row -->
+        </div>
+
+        <!-- Side Widget Well -->
+        <div class="well">
+            <h4>Side Widget Well</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
+        </div>
+
+    </div>
