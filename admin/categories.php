3702ada273139b5d065c32408f6ea2f1d9f2fc81
Rimantas55 <43905835+Rimantas55@users.noreply.github.com>
2019-03-23 22:01:51 +0200

    added php to brinig some categories data

--- admin/categories.php
+++ admin/categories.php
@@ -39,6 +39,14 @@
 
 
                     <div class="col-xs-6">
+
+                    <?php
+
+                    $query = "SELECT * FROM categories";
+                    $select_categories = mysqli_query($connection, $query);
+
+                    ?>
+
                         <table class="table table-bordered table-hover">
                             <thead>
                                 <tr>
@@ -47,10 +55,24 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td>Baseball Category</td>
-                                    <td>Basketball Category</td>
-                                </tr>
+
+
+
+                        <?php
+                        while ($row = mysqli_fetch_assoc($select_categories)) {
+                            
+                            $cat_id = $row['cat_id'];
+                            $cat_title = $row['cat_title'];
+
+                            echo "<tr>";
+                            echo "<td>{$cat_id}</td>";
+                            echo "<td>{$cat_title}</td>";
+                            echo "</tr>";
+
+                        }
+
+                        ?>
+                                
                             </tbody>
                         </table>
 
