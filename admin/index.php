e56b95db91934f2fcf3c2b6eb7458cd16e22b64c
Rimantas55 <43905835+Rimantas55@users.noreply.github.com>
2019-03-23 21:42:47 +0200

    chagend file name to admin_header footer navigation

--- admin/index.php
+++ admin/index.php
@@ -1,9 +1,9 @@
-<?php include "includes/header.php";?>
+<?php include "includes/admin_header.php";?>
 
 <div id="wrapper">
 
     <!-- Navigation -->
-<?php include "includes/navigation.php";?> 
+<?php include "includes/admin_navigation.php";?> 
 
 
 
@@ -44,4 +44,4 @@
 
     <!-- /#page-wrapper -->
 
-<?php include "includes/footer.php";?> 
+<?php include "includes/admin_footer.php";?> 
