3ae0e9dd4ba0a5fa6d2ca1c343827095932a904b
Rimantas55 <43905835+Rimantas55@users.noreply.github.com>
2019-03-25 20:18:06 +0200

    updating or editing categories part 1

--- admin/categories.php
+++ admin/categories.php
@@ -60,9 +60,29 @@
                         <div class="form-group">
                             <input type="submit" name="submit" class="btn btn-primary" value="Add Category">
                         </div>
-
-
                     </form>
+
+                   <form action="" method="post">
+                        <div class="form-group">
+                            <label for="cat_title">Edit Category</label>
+                            <input type="text" name="cat_title" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" name="submit" class="btn btn-primary" value="Update Category">
+                        </div>
+                    </form>
+
+
+
+
+
+
+
+
+
+
+
+
                 </div><!-- Add Category Form -->
 
                 <div class="col-xs-6">
