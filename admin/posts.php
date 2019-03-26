f768d1bf6f59f22ee14dcf6cc943a61edb3e3131
Rimantas55 <43905835+Rimantas55@users.noreply.github.com>
2019-03-26 20:11:04 +0200

    Creating a HTML table in admin to display a list of posts

---
+++ admin/posts.php
@@ -0,0 +1,62 @@
+<?php include "includes/admin_header.php";?>
+
+<div id="wrapper">
+
+    <!-- Navigation -->
+    <?php include "includes/admin_navigation.php";?> 
+
+
+    <div id="page-wrapper">
+
+        <div class="container-fluid">
+
+            <!-- Page Heading -->
+            <div class="row">
+                <div class="col-lg-12">
+
+                    <h1 class="page-header">
+                        WELLCOME TO ADMIN
+                        <small>Author</small>
+
+                        <table class="table table-bordered table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Author</th>
+                                    <th>Title</th>
+                                    <th>Category</th>
+                                    <th>Status</th>
+                                    <th>Image</th>
+                                    <th>Tags</th>
+                                    <th>Comments</th>
+                                    <th>Date</th>
+                                </tr>
+                            </thead>
+                        </table>
+
+
+
+
+
+
+
+                    </h1>
+
+            </div>
+        </div><!-- /.row -->
+        
+
+    </div><!-- /.container-fluid -->
+    
+
+</div>
+
+
+
+
+
+
+
+<!-- /#page-wrapper -->
+
+<?php include "includes/admin_footer.php";?> 
