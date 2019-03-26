e32cc2b64278b62bd3d17ff476c087410b555e16
Rimantas55 <43905835+Rimantas55@users.noreply.github.com>
2019-03-26 19:35:48 +0200

    refactoring part 1 create functions.php, dont forget global $connection, and include files

--- admin/categories.php
+++ admin/categories.php
@@ -23,36 +23,9 @@
                     <div class="col-xs-6">
 
 
-                        <?php 
+                    <?php insert_categories(); ?>
 
-                        if(isset($_POST['submit'])) {
-
-                         $cat_title = $_POST['cat_title'];
-
-                         if($cat_title == "" || empty($cat_title)) {
-
-                            echo "<div class='alert alert-danger'><h3>This field should not be empty</h3></div>";
-
-                        } else {
-
-                            $query = "INSERT INTO categories(cat_title)";
-                            $query .= "VALUE('{$cat_title}') ";
-
-                            $create_category_query = mysqli_query($connection, $query);
-
-                            if(!$create_category_query) {
-
-                                die("QUERY FAILED" . mysqli_error($connection));
-
-                            }
-
-                        }
-
-                    }
-
-                    ?>
-
-
+                    
                     <form action="" method="post">
                         <div class="form-group">
                             <label for="cat_title">Add Category</label>
